3a8588611150b9fc3d3fe107f9d8f53b14f29555
Sebastian Rehm <rehmsebastian@gmail.com>
2018-08-19 13:38:50 +0200

    Remove gatsby plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,18 +6,6 @@ module.exports = {
     'gatsby-plugin-styled-components',
     'gatsby-plugin-react-helmet',
     {
-      resolve: `gatsby-plugin-manifest`,
-      options: {
-        name: 'gatsby-starter-default',
-        short_name: 'starter',
-        start_url: '/',
-        background_color: '#33B67A',
-        theme_color: '#33B67A',
-        display: 'minimal-ui',
-        icon: 'src/images/apple-touch-icon.png', // This path is relative to the root of the site.
-      },
-    },
-    {
       resolve: `gatsby-source-iubenda`,
       options: {
         documentIds: ['7947842'],
